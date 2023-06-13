f797d4561da0df5929eeb5f3ca8bc7802955b765
303B-14\cre <a>
2023-06-13 17:10:50 +0900

    a

--- page-recruit.php
+++ page-recruit.php
@@ -9,7 +9,7 @@ get_header();
   <section>
     <h2>コンセプト</h2>
     <div>
-      <img src="<?php echo get_theme_file_uri( '画像パス' ); ?>" alt="" />
+    <img src="<?php echo get_theme_file_uri( '画像パス' ); ?>" alt="" />
     </div>
     <p>文章が入ります</p>
   </section>
